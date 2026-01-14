1a48e11477e71a83af817514400fd736d9427a3f
iwasse <guilherme.iwasse@gmail.com>
2026-01-14 13:32:02 -0300

    Upgrade to v4 artifact actions

--- src/test/java/com/iwasse/BaseWeb.java
+++ src/test/java/com/iwasse/BaseWeb.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
-
 import java.time.Duration;
 
 @Listeners({TestListener.class})
